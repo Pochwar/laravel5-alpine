9419e692d46d6de154c5049d8d09792dd745464a
Benoît RIPOCHE <benoit.ripoche@smile.fr>
2019-03-20 13:51:23 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM alpine:3.9
+
+# Install php and extensions
+RUN apk update && apk upgrade
+RUN apk add curl php7 php7-json php7-phar php7-mbstring php7-openssl php7-zip php7-pdo php7-bcmath php7-dom php7-xml php7-xmlwriter php7-tokenizer php7-pdo_mysql php7-fileinfo php7-session
+
+# Install composer
+RUN curl -s https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer
+
+# Install Node and NPM
+RUN apk add nodejs npm
+
+EXPOSE 2440
+
+WORKDIR /app
+VOLUME /app
+COPY docker/startup.sh /startup.sh
+
+ENTRYPOINT [ "/startup.sh" ]
---
+++ startup.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+
+#########################################################
+#                                                       #
+# This script is highly inspired by the one             #
+# of the bitnami-docker-laravel image                   #
+#                                                       #
+# https://github.com/bitnami/bitnami-docker-laravel     #
+#                                                       #
+#########################################################
+
+INIT_SEM=/tmp/initialized.sem
+
+fresh_container() {
+  [[ ! -f ${INIT_SEM} ]]
+}
+
+app_present() {
+  [[ -f /app/config/database.php ]]
+}
+
+wait_for_db() {
+  local db_host="${DB_HOST:-mariadb}"
+  local db_port="${DB_PORT:-3306}"
+  local db_address=$(getent hosts "$db_host" | awk '{ print $1 }')
+  counter=0
+  echo "Connecting to mariadb at $db_address"
+  while ! curl --silent "$db_address:$db_port" >/dev/null; do
+    counter=$((counter+1))
+    if [[ $counter == 30 ]]; then
+      echo "Error: Couldn't connect to mariadb."
+      exit 1
+    fi
+    echo "Trying to connect to mariadb at $db_address. Attempt $counter."
+    sleep 5
+  done
+}
+
+create_env_file() {
+  echo "Create .env file"
+  cp .env.example .env
+  sed -i 's/\(APP_URL=\).*/\1http:\/\/localhost:2440/' .env
+  php artisan key:generate
+  php artisan jwt:secret
+}
+
+setup_db() {
+  echo "Configuring the database"
+  sed -i "s/utf8mb4/utf8/g" /app/config/database.php
+  php artisan migrate --force
+}
+
+echo "Installing/Updating Laravel dependencies (composer)"
+composer install
+echo "Laravel dependencies installed/updated"
+
+if [[ -f ".env" ]];then
+  echo ".env file already exists"
+else
+  create_env_file
+fi
+
+wait_for_db
+
+if ! fresh_container; then
+  echo "#########################################################################"
+  echo "                                                                         "
+  echo " App initialization skipped:                                             "
+  echo " Delete the file $INIT_SEM and restart the container to reinitialize     "
+  echo " You can alternatively run specific commands using docker-compose exec   "
+  echo " e.g docker-compose exec myapp php artisan make:console FooCommand       "
+  echo "                                                                         "
+  echo "#########################################################################"
+else
+  setup_db
+  echo "Initialization finished"
+  touch ${INIT_SEM}
+fi
+
+echo "Installing/Updating Front dependencies (npm)"
+npm i
+echo "Front dependencies installed/updated"
+
+echo "Build front"
+npm run dev
+echo "Front build"
+
+php artisan serve --host=0.0.0.0 --port=2440
